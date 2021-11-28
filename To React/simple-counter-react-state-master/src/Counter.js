47977c10b5dcea4a90544c8bb7c865d169403e7f
Manish Sajwan <manish.sajwan@protonmail.com>
2021-11-28 21:38:11 +0530

    set up simple counter react app

---
+++ To React/simple-counter-react-state-master/src/Counter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {this.setState({ count: this.state.count + 1 });}
+  decrement = () => this.setState({ count: this.state.count - 1 });
+  reset = () => this.setState({ count: 0 });
+  
+  render() {
+    const { count } = this.state;
+    
+    return (
+      <div className="Counter">
+        <p className="count">{count}</p>
+        <section className="controls">
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          <button onClick={this.reset}>Reset</button>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ To React/simple-counter-react-state-master/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Counter from './Counter';
+
+const Application = () => {
+  return (
+    <main className="Application">
+      <section className="Counters">
+        <Counter />
+      </section>
+    </main>
+  );
+};
+
+ReactDOM.render(<Application />, document.getElementById('root'));
